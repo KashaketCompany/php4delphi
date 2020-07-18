713af405ea5a96ca8293fbf92ca5fb50d19f2f5e
lion <levzenin@pm.me>
2020-07-18 18:10:19 +0300

    php4application -> library: moved to another branch
    Added pre-compiled binaries:
    v {5, 5.0.4,  5.1.1, 5.2+, 5.3+, 5.4+, 5.5+, 5.6+}

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# php4application
+
+This folder contains pre-compiled PHP4Applications library
--- Source/Clear.bat
+++
@@ -1,8 +0,0 @@
-del /f php4delphi.dcu
-del /f phpFunctions.dcu
-del /f DelphiFunctions.dcu
-del /f PHPCommon.dcu
-del /f PHPAPI.dcu
-del /f ZENDAPI.dcu
-del /f PHPTypes.dcu
-del /f ZendTypes.dcu
